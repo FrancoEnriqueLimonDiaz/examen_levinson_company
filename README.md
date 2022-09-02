c82942eb4392e72ce7c534df2c8c2d2d5b91c86e
Franco Limon <francoenriquee5@gmail.com>
2022-09-01 23:33:07 -0500

    commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # examen_levinson_company
 Examen de admission para Levinson company
+
+```bash
+  #Run
+  npm install
+  #Run
+  nodemon app
+  #server in por 3010
+  http://localhost:3010/
+```
--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+const express = require("express");
 var fetch = require("node-fetch");
-var result = [];
+const app = express();
+let array = [];
+
 fetch("https://kpq7uxowd1.execute-api.us-east-2.amazonaws.com/dev/examen1", 
 {
   headers: ({
     'Authorization': 'G9zn5KTz8y2PMll11TXx', 
     'Content-Type': 'application/x-www-form-urlencoded'
   }), 
-}
-)
-.then(res => res.json())
-.then(json => {
-  var obj_json = JSON.parse(json)
-  console.log(obj_json.length)
 })
+.then((response) => response.json())
+.then((data) => {
+  var set_entries = Object.values(data)
+  set_entries.forEach(element => {
+    array.push(element);
+  });
+})
+
+app.listen(3010, () => {
+  console.log("Starting exam in 3010");
+});
+
+app.get("/", (req, res) => {
+  res.send(`<h1 id="hello">${array}</h1>`);
+});
