42019155a341987d09135faefc1e85265acc9b25
Franco Limon <francoenriquee5@gmail.com>
2022-09-02 17:10:50 -0500

    commit

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require("express");
 var fetch = require("node-fetch");
 const app = express();
-let by_Make = []
-let by_Model = []
-let by_Color = []
-let type_cars = []
-let cars = []
-const count1= {};
-const count2 = {};
-const count3 = {};
+const by_make= {};
+let names = [];
+
 
 fetch("https://kpq7uxowd1.execute-api.us-east-2.amazonaws.com/dev/examen3", 
 {
@@ -19,37 +14,22 @@ fetch("https://kpq7uxowd1.execute-api.us-east-2.amazonaws.com/dev/examen3",
 })
 .then((response) => response.json())
 .then((data) => {
-  // console.log(data)
-  for (let index = 0; index < data.length; index++) {
-    by_Make = [...by_Make, data[index].make];
-    by_Model = [...by_Model, data[index].model];
-    by_Color = [...by_Color, data[index].color];
+  //get the name of each element
+  data.forEach(element => {
+    names.push(element.make)
+  });
+  //get the number of repetition.
+  names.forEach(element => {
+    by_make[element] = (by_make[element] || 0) + 1;
+  });
+  //add the names into object.
+  var exam = { 
+    by_make
   }
-  type_cars = [...type_cars, by_Make, by_Model, by_Color]
-  type_cars[0].forEach(element => {
-    count1[element] = (count1[element] || 0) + 1;
-  });
-  type_cars[1].forEach(element => {
-    count2[element] = (count2[element] || 0) + 1;
-  });
-  type_cars[2].forEach(element => {
-    count3[element] = (count3[element] || 0) + 1;
-  });
-  var set_entrie1 = Object.entries(count1)
-  var set_entrie2 = Object.entries(count2)    
-  console.log(set_entrie1)
-  console.log(set_entrie2)
-  // console.log(Object.entries(count3))
-  // const by_make = JSON.stringify(count1)
-  // const by_model = JSON.stringify(count2)
-  // const by_color = JSON.stringify(count3)
-  //tenemos los tipos
-  // console.log(by_make)
-  // console.log(by_model)
-  // console.log(by_color)
-  //creamos los grupos
-
+  console.log(exam)
 })
+
+
 
 
 
